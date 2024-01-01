5d1dda54020a1a5c8e39de2f4579d77818ffabe1
Mugniraha <mughnirahadiansah@gmail.com>
2024-01-01 19:46:48 +0700

    perbaikan harga bahan custom harga

--- database/migrations/2024_01_01_121055_create_bahan_custom_table.php
+++ database/migrations/2024_01_01_121055_create_bahan_custom_table.php
@@ -4,21 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBahanTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
         Schema::create('bahan', function (Blueprint $table) {
             $table->id('idBahan');
-            $table->float('besi')->default(0);
-            $table->float('stainless_steel')->default(0);
-            $table->float('alumunium')->default(0);
-            $table->float('baja_ringan')->default(0);
+            $table->string('namaBahan')->default(0);
+            $table->float('hargaBahan')->default(0);
 
             // Tambahkan kolom lain sesuai kebutuhan
 
@@ -28,11 +24,11 @@ class CreateBahanTable extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
         Schema::dropIfExists('bahan');
     }
-}
+};
+
+
